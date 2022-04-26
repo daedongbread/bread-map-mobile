b1608226235b640ece9c6f1d59c51fb1abee06f1
kjoobin <imkj1148@gmail.com>
2022-04-26 23:09:16 +0900

    Add android splash screen

--- android/app/src/main/java/com/daedongbread/MainActivity.java
+++ android/app/src/main/java/com/daedongbread/MainActivity.java
@@ -1,9 +1,11 @@
 package com.daedongbread;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,7 +17,13 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "daedongbread";
   }
-    @Override
+
+   protected void onCreate(Bundle savedInstanceState) {
+       SplashScreen.show(this);  // here
+       super.onCreate(savedInstanceState);
+   }
+
+  @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
       @Override
