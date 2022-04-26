068e72310c57a64202d9146ffb7ba8a8de1f92ab
kjoobin <imkj1148@gmail.com>
2022-04-26 23:11:29 +0900

    Add splash provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AuthProvider } from '@/provider/AuthProvider/AuthProvider';
+import { SplashProvider } from '@/provider/SplashProvider';
 import { Navigation } from '@/router';
 import { theme } from '@/styles/theme';
 import { bakeryMenu, reviews, info } from '@/utils/tempData';
@@ -9,13 +10,15 @@ import { ThemeProvider } from '@emotion/react';
 const queryClient = new QueryClient();
 
 const App: FC = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <AuthProvider>
-        <Navigation data={{ bakeryMenu, reviews, info }} />
-      </AuthProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
+  <SplashProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <Navigation data={{ bakeryMenu, reviews, info }} />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </SplashProvider>
 );
 
 export default App;
