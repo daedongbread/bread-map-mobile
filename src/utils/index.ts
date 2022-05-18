a9e50ae3fe19c2a2bf1227c6e095bdeb777f2100
kjoobin <imkj1148@gmail.com>
2022-05-18 19:47:23 +0900

    Add numberFormat

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export { bindHook } from './bindHook';
 export { Config } from './config';
 export { resizePixel, resizePixels } from './resizePixel';
+export * from './numberFormat';
 export { bakeryMenu, bakeryReviews, bakeryInfo, bakeryList } from './tempData';
 export type { BakeryMenu, BakeryReview, BakeryInfo } from './tempData'; // TODO: 타입 선언 옮기기
