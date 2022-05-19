df976555ce643f6a30452fa9d700ffd7ae58f2bd
kjoobin <imkj1148@gmail.com>
2022-05-19 22:47:02 +0900

    Refactor memorize text component

--- src/components/Shared/Text/Text.tsx
+++ src/components/Shared/Text/Text.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { memo, useCallback } from 'react';
 import { TextProps, Text as ReactNativeText } from 'react-native';
 import { presets as textPresets, TextPresets } from '@shared/Text/presets';
 
@@ -6,7 +6,7 @@ type Props = TextProps & {
   presets?: TextPresets | Array<TextPresets>;
 };
 
-export const Text: React.FC<Props> = ({ presets = 'body1', style: styleOverride, ...rest }) => {
+export const Text: React.FC<Props> = memo(({ presets = 'body1', style: styleOverride, ...rest }) => {
   const parsingPresets = useCallback(() => {
     if (typeof presets === 'object') {
       return presets.map(item => textPresets[item]);
@@ -18,4 +18,4 @@ export const Text: React.FC<Props> = ({ presets = 'body1', style: styleOverride,
   const styles = [...parsingPresets(), styleOverride];
 
   return <ReactNativeText style={styles} {...rest} />;
-};
+});
