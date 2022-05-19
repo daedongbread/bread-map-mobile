19deef5808e87dcbad6e3d1ad3b42e25d170064b
kjoobin <imkj1148@gmail.com>
2022-05-19 22:41:14 +0900

    Add text HOC with presets

---
+++ src/components/Shared/Text/Text.tsx
@@ -0,0 +1,21 @@
+import React, { useCallback } from 'react';
+import { TextProps, Text as ReactNativeText } from 'react-native';
+import { presets as textPresets, TextPresets } from '@shared/Text/presets';
+
+type Props = TextProps & {
+  presets?: TextPresets | Array<TextPresets>;
+};
+
+export const Text: React.FC<Props> = ({ presets = 'body1', style: styleOverride, ...rest }) => {
+  const parsingPresets = useCallback(() => {
+    if (typeof presets === 'object') {
+      return presets.map(item => textPresets[item]);
+    }
+
+    return [textPresets[presets]];
+  }, [presets]);
+
+  const styles = [...parsingPresets(), styleOverride];
+
+  return <ReactNativeText style={styles} {...rest} />;
+};
