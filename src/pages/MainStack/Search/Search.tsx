1dbe9e8eb734f13084292197dfd015927c78957b
kjoobin <imkj1148@gmail.com>
2022-07-17 20:58:23 +0900

    Add search history feature

--- src/pages/MainStack/Search/Search.tsx
+++ src/pages/MainStack/Search/Search.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SearchBakeryList } from '@/components/Search/SearchBakeryList';
+import { SearchHistoryList } from '@/components/Search/SearchHistoryList';
+import { Header } from '@/pages/MainStack/Search/Header';
+import { MainStackScreenProps } from '@/pages/MainStack/Stack';
+import { storage } from '@/utils/storage/storage';
 import { Text } from '@shared/Text';
 
-const Search = () => {
+type Props = MainStackScreenProps<'Search'>;
+
+const Search: React.FC<Props> = ({ navigation }) => {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchHistory, setSearchHistory] = useState<Array<string>>([]);
+  const [bakeryList, setBakeryList] = useState();
+
+  const onPress = useCallback(() => {
+    if (navigation.canGoBack()) {
+      navigation.pop();
+    }
+  }, [navigation]);
+
+  useEffect(() => {
+    const getSearchHistory = async () => {
+      const res = await storage.get('search');
+      if (!res) {
+        return;
+      }
+
+      return JSON.parse(res);
+    };
+
+    getSearchHistory().then(setSearchHistory);
+  }, []);
+
   return (
-    <SafeAreaView style={[styles.fullScreen, styles.container]}>
-      <View style={styles.fullScreen}>
-        <Text>Search</Text>
+    <SafeAreaView style={[styles.fullScreen]}>
+      <Header value={searchValue} onChangeText={setSearchValue} onPress={onPress} />
+      <View style={styles.container}>
+        {bakeryList ? (
+          <SearchBakeryList bakeries={bakeryList} />
+        ) : (
+          <>
+            <Text presets={['body1', 'bold']} style={styles.historyTitle}>
+              최근검색
+            </Text>
+            <SearchHistoryList searchHistory={searchHistory} />
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 32,
-    paddingHorizontal: 32,
-  },
   fullScreen: {
     flex: 1,
+  },
+  container: {
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+  },
+  historyTitle: {
+    marginBottom: 16,
   },
 });
 
