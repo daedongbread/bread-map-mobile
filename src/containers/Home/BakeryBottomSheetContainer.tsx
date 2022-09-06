3e865520e2b167e82daeb50bd0f83555f582b78a
kjoobin <imkj1148@gmail.com>
2022-09-07 08:52:16 +0900

    feat: add bakery map slice

--- src/containers/Home/BakeryBottomSheetContainer.tsx
+++ src/containers/Home/BakeryBottomSheetContainer.tsx
@@ -5,7 +5,7 @@ import { useGetBakeries } from '@/apis';
 import { BakeryMapBakeryEntity } from '@/apis/bakery/types';
 import { BakeriesBottomSheet } from '@/components/Home/BakeriesBottomSheet';
 
-import { useGeolocation } from '@/hooks/useGeolocation';
+import { useAppSelector } from '@/hooks/redux';
 import { HomeStackScreenProps } from '@/pages/MainStack/MainTab/HomeStack/Stack';
 import { bakeryInfo, bakeryMenu, bakeryReviews } from '@/utils';
 import { useNavigation } from '@react-navigation/native';
@@ -15,7 +15,7 @@ const bakeryData = { bakeryMenu, bakeryReviews, bakeryInfo };
 export type TabItem = 'distance' | 'popularity';
 
 export const BakeryBottomSheetContainer: React.VFC = () => {
-  const { currentPosition } = useGeolocation();
+  const { searchMapCameraLocation } = useAppSelector(select => select.bakeryMap);
 
   const [activeTab, setActiveTab] = useState<TabItem>('distance');
 
@@ -50,10 +50,10 @@ export const BakeryBottomSheetContainer: React.VFC = () => {
 
   const { bakeries } = useGetBakeries({
     sort: 'distance',
-    latitude: currentPosition?.latitude,
-    longitude: currentPosition?.longitude,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.02,
+    latitude: searchMapCameraLocation?.latitude,
+    longitude: searchMapCameraLocation?.longitude,
+    latitudeDelta: searchMapCameraLocation?.latitudeDelta,
+    longitudeDelta: searchMapCameraLocation?.longitudeDelta,
   });
 
   return (
