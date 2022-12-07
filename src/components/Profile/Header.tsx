693aa4cab3e9d68d868302202e460945de70ed0e
KJoobin <imkj1148@gmail.com>
2022-12-07 20:42:48 +0900

    Feat: setting page

--- src/components/Profile/Header.tsx
+++ src/components/Profile/Header.tsx
@@ -23,6 +23,10 @@ export const Header = ({ type, title, onClickRight }: Props) => {
     navigation.pop();
   };
 
+  const navigateSetting = () => {
+    navigation.push('SettingModal');
+  };
+
   if (type === 'ME') {
     return (
       <View style={styles.MeContainer}>
@@ -30,7 +34,7 @@ export const Header = ({ type, title, onClickRight }: Props) => {
           <IcArea />
         </TouchableOpacity>
         <SplitColumn width={16} />
-        <TouchableOpacity>
+        <TouchableOpacity onPress={navigateSetting}>
           <IcSetting />
         </TouchableOpacity>
       </View>
---
+++ src/pages/MainStack/BlockList/BlockList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from '@shared/Text';
+export const BlockList = () => {
+  return (
+    <View>
+      <Text>BlockList</Text>
+    </View>
+  );
+};
