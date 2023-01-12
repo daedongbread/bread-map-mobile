61ed2fc33b55b31c9beb98590e7e8bcfa4238e32
yunyeop <yykim000922@gmail.com>
2023-01-12 21:48:07 +0900

    feat : 홈 탭 주소, 전화번호클립보드 복사 기능 구현

--- src/components/BakeryDetail/Home/RowInfo.tsx
+++ src/components/BakeryDetail/Home/RowInfo.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { CopyButton } from '@/components/Shared/Button/CopyButton';
+import { SplitColumn } from '@/components/Shared/SplitSpace';
+import { showToast } from '@/slices/toast';
 import { theme } from '@/styles/theme';
 import { resizePixels } from '@/utils';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 type Props = {
   icon: React.ReactNode;
   text: string;
+  isCopyable?: boolean;
 };
 
-export const RowInfo: React.FC<Props> = ({ icon, text }) => (
-  <View style={styles.row}>
-    {icon}
-    <Text style={styles.text}>{text}</Text>
-  </View>
-);
+export const RowInfo: React.FC<Props> = ({ icon, text, isCopyable = false }) => {
+  const dispatch = useDispatch();
+
+  const onPress = () => {
+    Clipboard.setString(text);
+    dispatch(showToast({ text: '복사가 완료되었습니다' }));
+  };
+
+  return (
+    <>
+      <View style={styles.row}>
+        {icon}
+        <Text style={styles.text}>{text}</Text>
+        <SplitColumn width={10} />
+        {isCopyable && <CopyButton onPress={onPress} />}
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create(
   resizePixels({
