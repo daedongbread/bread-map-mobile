106b0aee7037c91a577ee8562e7b8ee0536cab8f
KJoobin <imkj1148@gmail.com>
2023-01-19 00:51:41 +0900

    Feat: fix marker icon

--- src/components/Home/BakeryMarker/BakeryMarker.tsx
+++ src/components/Home/BakeryMarker/BakeryMarker.tsx
@@ -3,7 +3,6 @@ import { Marker } from 'react-native-maps';
 
 import { BakeryMapBakeryEntity } from '@/apis/bakery/types';
 import { flagColorHexColors } from '@/containers/Bookmark';
-import styled from '@emotion/native';
 import { BreadCakeIcon, HeartIcon } from '@shared/Icons';
 import IcHeart32 from '@shared/Icons/IcHeart32.svg';
 import IcSelectedMapPin from '@shared/Icons/IcSelectedMapPin.svg';
@@ -13,43 +12,47 @@ type Props = {
   bakeryMapEntity: BakeryMapBakeryEntity;
   onPress: (bakeryMapEntity?: BakeryMapBakeryEntity) => void;
   color?: string;
+  markerIcon: 'default' | 'saved';
 };
 
-const BakeryMarker: React.FC<Props> = React.memo(({ activeMarkerId, bakeryMapEntity, onPress, color }) => {
-  const isActive = bakeryMapEntity.id === activeMarkerId;
+const DEFAULT_ICON_COLOR = '#FF6E40';
 
-  const handlePress = useCallback(() => {
-    if (isActive) {
-      onPress();
-      return;
+export const BakeryMarker: React.FC<Props> = React.memo(
+  ({ activeMarkerId, bakeryMapEntity, onPress, color, markerIcon }) => {
+    const isActive = bakeryMapEntity.id === activeMarkerId;
+
+    const handlePress = useCallback(() => {
+      if (isActive) {
+        onPress();
+        return;
+      }
+
+      onPress(bakeryMapEntity);
+    }, [bakeryMapEntity, isActive, onPress]);
+
+    if (markerIcon === 'saved') {
+      const flagColor = color as keyof typeof flagColorHexColors;
+      const iconColor = flagColorHexColors[flagColor];
+
+      if (color === 'GRAY') {
+        return (
+          <Marker coordinate={bakeryMapEntity} onPress={handlePress}>
+            {isActive ? <IcSelectedMapPin color={iconColor} /> : <BreadCakeIcon color={iconColor} />}
+          </Marker>
+        );
+      }
+
+      return (
+        <Marker coordinate={bakeryMapEntity} onPress={handlePress}>
+          {isActive ? <IcHeart32 color={iconColor} /> : <HeartIcon color={iconColor} />}
+        </Marker>
+      );
     }
-
-    onPress(bakeryMapEntity);
-  }, [bakeryMapEntity, isActive, onPress]);
-
-  if (color && color in flagColorHexColors) {
-    const flagColor = color as keyof typeof flagColorHexColors;
 
     return (
       <Marker coordinate={bakeryMapEntity} onPress={handlePress}>
-        {isActive ? (
-          <IcHeart32 color={flagColorHexColors[flagColor]} />
-        ) : (
-          <HeartIcon color={flagColorHexColors[flagColor]} />
-        )}
+        {isActive ? <IcSelectedMapPin color={DEFAULT_ICON_COLOR} /> : <BreadCakeIcon color={DEFAULT_ICON_COLOR} />}
       </Marker>
     );
   }
-
-  return (
-    <Marker coordinate={bakeryMapEntity} onPress={handlePress}>
-      {isActive ? <IcSelectedMapPin /> : <IconStyle />}
-    </Marker>
-  );
-});
-
-export { BakeryMarker };
-
-const IconStyle = styled(BreadCakeIcon)`
-  color: ${({ theme }) => theme.color.white};
-`;
+);
--- src/components/Shared/Icons/BreadCakeIcon.tsx
+++ src/components/Shared/Icons/BreadCakeIcon.tsx
@@ -5,7 +5,7 @@ export const BreadCakeIcon: FC<ComponentProps<typeof Svg>> = props => (
   <Svg viewBox={'0 0 16 18'} width={16} height={18} fill="none" {...props}>
     <Path
       d="M16 7.75C16 12.0302 10.3538 18 8 18C5.64624 18 0 12.0302 0 7.75C0 3.46979 3.58172 0 8 0C12.4183 0 16 3.46979 16 7.75Z"
-      fill="#FF6E40"
+      fill="currentColor"
     />
     <Path
       fill-rule="evenodd"
