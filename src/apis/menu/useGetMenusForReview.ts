654acc3a6e2c8ecdc5101a3663752dce6f456912
yunyeop <yykim000922@gmail.com>
2023-02-13 23:11:48 +0900

    refactor : 메뉴 api, entity 변경

---
+++ src/apis/menu/useGetMenusForReview.ts
@@ -0,0 +1,29 @@
+import { useQuery } from 'react-query';
+import { fetcher } from '../fetcher';
+import { MenuForReviewEntity } from './type';
+
+type GetMenusForReviewRes = {
+  data: MenuForReviewEntity[];
+};
+
+type UseGetMenusForReviewProps = {
+  bakeryId: number;
+};
+
+const getMenusForReview = async ({ bakeryId }: UseGetMenusForReviewProps) => {
+  const { data } = await fetcher.get<GetMenusForReviewRes>(`/bakery/${bakeryId}/review/product/search?name=`, {});
+  return data.data;
+};
+
+export const useGetMenusForReview = ({ bakeryId }: UseGetMenusForReviewProps) => {
+  const { data, isLoading, isError, refetch } = useQuery(['useGetBreads', { bakeryId }], () =>
+    getMenusForReview({ bakeryId })
+  );
+
+  return {
+    menus: data,
+    isLoading,
+    isError,
+    refetch,
+  };
+};
