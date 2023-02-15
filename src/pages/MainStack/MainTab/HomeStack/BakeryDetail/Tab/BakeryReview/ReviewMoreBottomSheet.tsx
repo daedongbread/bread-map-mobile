18ce5a383614f81e9d0aee6dc6deaeb0a2c73046
yunyeop <yykim000922@gmail.com>
2023-02-15 19:21:28 +0900

    fix : BottomSheet close시 부모 페이지 까지 닫히는 현상 수정

--- src/pages/MainStack/MainTab/HomeStack/BakeryDetail/Tab/BakeryReview/ReviewMoreBottomSheet.tsx
+++ src/pages/MainStack/MainTab/HomeStack/BakeryDetail/Tab/BakeryReview/ReviewMoreBottomSheet.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Modal } from 'react-native';
 import { ReviewMoreBottomSheetContainer } from '@/containers/BakeryDetail/BakeryMenu';
+import { useIsFocused } from '@react-navigation/native';
 
 export const ReviewMoreBottomSheet = () => {
-  return <ReviewMoreBottomSheetContainer />;
+  const focused = useIsFocused();
+
+  return (
+    <Modal visible={focused} animationType={'fade'} transparent statusBarTranslucent>
+      <ReviewMoreBottomSheetContainer />
+    </Modal>
+  );
 };
