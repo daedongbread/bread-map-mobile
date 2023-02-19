9e085b3c5cc9118a4c211540f9857418a71baa52
yunyeop <yykim000922@gmail.com>
2023-02-19 17:47:46 +0900

    fix : 리뷰작성>리뷰선택 화면에서 close버튼 클릭시 questionPopup navigation 연동

--- src/components/BakeryDetail/BakeryReview/ReviewWrite/ReviewSelect/ReviewSelectComponent.tsx
+++ src/components/BakeryDetail/BakeryReview/ReviewWrite/ReviewSelect/ReviewSelectComponent.tsx
@@ -4,8 +4,10 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { MenuForReviewEntity } from '@/apis/menu/type';
 import { Button } from '@/components/Shared/Button/Button';
 import { Header } from '@/components/Shared/Header';
+import { ReviewWriteStackNavigationProps } from '@/pages/ReviewWriteStack/Stack';
 import { RatedBread } from '@/slices/reviewWrite';
 import { theme } from '@/styles/theme';
+import { useNavigation } from '@react-navigation/native';
 import { BreadToggleList } from './BreadToggleList';
 import { ContentsHeader } from './ContentsHeader';
 import { ContentsList } from './ContentsList';
@@ -24,6 +26,8 @@ type Props = {
   closePage: () => void;
 };
 
+type Navigation = ReviewWriteStackNavigationProps<'ReviewSelect'>['navigation'];
+
 export const ReviewSelectComponent: React.FC<Props> = ({
   breads,
   searchValue,
@@ -36,10 +40,24 @@ export const ReviewSelectComponent: React.FC<Props> = ({
   isExistBread,
   closePage,
 }) => {
+  const navigation = useNavigation<Navigation>();
+
+  const onPressHeaderCloseButton = () => {
+    if (selectedBreads.length > 0) {
+      goNavQuestionBottomSheet();
+    } else {
+      closePage();
+    }
+  };
+
+  const goNavQuestionBottomSheet = () => {
+    navigation.navigate('QuestionBottomSheet');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Header title={'리뷰작성'} onPressClose={closePage} isCloseButtonShown />
+        <Header title={'리뷰작성'} onPressClose={onPressHeaderCloseButton} isCloseButtonShown />
         <BreadToggleList selectedBreads={selectedBreads} manualSelectedBreads={manualSelectedBreads} />
         <ReviewSearch searchValue={searchValue} onChangeSearchValue={onChangeSearchValue} />
       </View>
