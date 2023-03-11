bc8b3a71073ddcc0f79d03db62e7a3373d59425f
yunyeop <yykim000922@gmail.com>
2023-03-11 14:37:46 +0900

    fix : rollback

--- src/containers/Home/BakeryBottomSheetContainer.tsx
+++ src/containers/Home/BakeryBottomSheetContainer.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from 'react';
 
+import { useGetBakeries } from '@/apis';
+
 import { BakeryMapBakeryEntity } from '@/apis/bakery/types';
-import { useGetBakeriesFilter } from '@/apis/bakery/useGetBakeriesFilter';
 import { BakeriesBottomSheet } from '@/components/Home/BakeriesBottomSheet';
 
 import { useAppDispatch, useAppSelector } from '@/hooks/redux';
@@ -47,9 +48,8 @@ export const BakeryBottomSheetContainer: React.VFC = () => {
     });
   };
 
-  const { bakeries } = useGetBakeriesFilter({
+  const { bakeries } = useGetBakeries({
     sort,
-    filter: true,
     latitude: searchMapCameraLocation?.latitude,
     longitude: searchMapCameraLocation?.longitude,
     latitudeDelta: searchMapCameraLocation?.latitudeDelta,
@@ -61,7 +61,7 @@ export const BakeryBottomSheetContainer: React.VFC = () => {
       onClickBakery={onClickBakery}
       activeTab={sort}
       onPressTab={onPressTab}
-      bakeryList={selectedMarker ? bakeries?.filter(bakery => bakery.id === selectedMarker.id) : bakeries}
+      bakeryList={selectedMarker ? [selectedMarker] : bakeries}
       hasSelected={!!selectedMarker}
       onPressIcon={onPressIcon}
     />
