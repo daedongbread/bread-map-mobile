308feef689a0a692199f6f94520535861d98ecdb
sumin Bae <qotnals12345@naver.com>
2023-03-12 13:33:56 +0900

    임시 상세페이지 정보 수정 삭제

--- src/pages/MainStack/ProfileStack/Profile.tsx
+++ src/pages/MainStack/ProfileStack/Profile.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Button, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { ProfileContainer } from '@/containers/Profile';
-import { useNavigation, useNavigationState } from '@react-navigation/native';
-import { MainStackScreenProps } from '../Stack';
 
 const Profile = () => {
-  const navigation = useNavigation<MainStackScreenProps<'MainTab'>['navigation']>();
-  const NavigationKey = useNavigationState(state => state);
-  const onClickButton = () => {
-    navigation.push('EditBakeryStack', {
-      screen: 'EditBakery',
-      params: {
-        bakeryId: 30000000000003,
-        NavigationKey: NavigationKey.routes[0].key,
-      },
-    });
-  };
+  // const navigation = useNavigation<MainStackScreenProps<'MainTab'>['navigation']>();
+  // const NavigationKey = useNavigationState(state => state);
+  // const onClickButton = () => {
+  //   navigation.push('EditBakeryStack', {
+  //     screen: 'EditBakery',
+  //     params: {
+  //       bakeryId: 30000000000003,
+  //       NavigationKey: NavigationKey.routes[0].key,
+  //     },
+  //   });
+  // };
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Button title="임시 상세페이지 정보수정" onPress={onClickButton} />
+      {/* <Button title="임시 상세페이지 정보수정" onPress={onClickButton} /> */}
       <ProfileContainer />
     </SafeAreaView>
   );
