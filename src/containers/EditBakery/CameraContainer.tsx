1b5d2fab0ae4ee15f9c166abff28bc090fd4e515
sumin Bae <qotnals12345@naver.com>
2023-03-26 21:19:08 +0900

    fix:android 33 갤러리 불러오기 대응

--- src/containers/EditBakery/CameraContainer.tsx
+++ src/containers/EditBakery/CameraContainer.tsx
@@ -77,7 +77,10 @@ export function CameraContainer() {
   };
 
   async function hasAndroidPermission() {
-    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+    const permission =
+      Platform.Version >= 33
+        ? PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES
+        : PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
 
     const hasPermission = await PermissionsAndroid.check(permission);
     if (hasPermission) {
