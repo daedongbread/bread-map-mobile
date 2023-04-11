019cac4ae9b2f18ff4b464fee42660dd67f1aa2b
KJoobin <imkj1148@gmail.com>
2023-04-12 08:15:57 +0900

    Feat: remove react-native-push-notification

--- ios/daedongbread/AppDelegate.m
+++ ios/daedongbread/AppDelegate.m
@@ -2,7 +2,6 @@
 #import "AppDelegate.h"
 #import <GoogleMaps/GoogleMaps.h>
 #import "RNSplashScreen.h"
-#import <RNCPushNotificationIOS.h>
 
 #if RCT_DEV
 #import <React/RCTDevLoadingView.h>
@@ -20,7 +19,6 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#import <UserNotifications/UserNotifications.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -68,41 +66,7 @@ static void InitializeFlipper(UIApplication *application) {
   [self.window makeKeyAndVisible];
   [RNSplashScreen show];
 
-  // Define UNUserNotificationCenter
-  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-  center.delegate = self;
-
   return YES;
-}
-
-//Called when a notification is delivered to a foreground app.
--(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
-{
-  completionHandler(UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge);
-}
-
-// Required for the register event.
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
- [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-}
-// Required for the notification event. You must call the completion handler after handling the remote notification.
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
-{
-  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
-// Required for the registrationError event.
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
-{
- [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
-}
-// Required for localNotification event
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center
-didReceiveNotificationResponse:(UNNotificationResponse *)response
-         withCompletionHandler:(void (^)(void))completionHandler
-{
-  [RNCPushNotificationIOS didReceiveNotificationResponse:response];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,18 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '@/hooks/redux';
-import { updateDeviceToken } from '@/slices/notice';
 import { initNotification } from '@/utils/notification';
 
 export const useNotification = () => {
-  const { deviceToken } = useAppSelector(selector => selector.notice);
+  useAppSelector(selector => selector.notice);
 
-  const dispatch = useDispatch();
+  useDispatch();
 
-  initNotification({
-    onRegister({ token }) {
-      if (deviceToken !== token) {
-        dispatch(updateDeviceToken(token));
-      }
-    },
-  });
+  initNotification();
 };
