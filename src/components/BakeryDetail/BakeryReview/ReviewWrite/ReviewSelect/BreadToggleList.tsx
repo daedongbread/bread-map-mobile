1bb66106accd8c45fe44efe0d5b729aece153c49
yunyeop <yykim000922@gmail.com>
2023-04-13 01:59:52 +0900

    fix : #217

--- src/components/BakeryDetail/BakeryReview/ReviewWrite/ReviewSelect/BreadToggleList.tsx
+++ src/components/BakeryDetail/BakeryReview/ReviewWrite/ReviewSelect/BreadToggleList.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+import { useDidMountEffect } from '@/hooks/useDidMountEffect';
 import { RatedBread } from '@/slices/reviewWrite';
 import { BreadToggle } from './BreadToggle';
 
@@ -10,10 +11,27 @@ type Props = {
 };
 
 export const BreadToggleList: React.FC<Props> = ({ selectedBreads, manualSelectedBreads }) => {
+  const ref = useRef<FlatList>(null);
+  const [prevBreadsSize, setPrevBreadsSize] = useState(0);
   const renderData = [...selectedBreads, ...manualSelectedBreads];
+
+  const scrollToEnd = useCallback(() => {
+    setTimeout(() => {
+      ref.current?.scrollToEnd({ animated: true });
+    }, 100);
+  }, []);
+
+  useDidMountEffect(() => {
+    if (prevBreadsSize < renderData.length) {
+      scrollToEnd();
+    }
+
+    setPrevBreadsSize(renderData.length);
+  }, [selectedBreads, manualSelectedBreads]);
 
   return (
     <FlatList
+      ref={ref}
       data={renderData}
       contentContainerStyle={styles.container}
       horizontal
