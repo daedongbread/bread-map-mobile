0fb6f7e7ef1718b1d21d5f8e4a3e52106636d0ee
yunyeop <yykim000922@gmail.com>
2023-08-11 21:49:24 +0900

    feat : 신고하기 기능 api  구현

--- src/apis/community/usePostAccusePost.ts
+++ src/apis/community/usePostAccusePost.ts
@@ -1,8 +1,19 @@
 import { useMutation } from 'react-query';
 import { fetcher } from '../fetcher';
+import { PostTopic } from './types';
 
-const accusePost = () => {
-  return fetcher.post('/v1/reports//', {});
+type UsePostAccusePostRequest = {
+  postTopic: PostTopic;
+  targetId: number;
+  reason: string;
+  content: string;
+};
+
+const accusePost = ({ postTopic, targetId, content, reason }: UsePostAccusePostRequest) => {
+  return fetcher.post(`/v1/reports/${postTopic}/${targetId}`, {
+    reason,
+    content,
+  });
 };
 
 export const usePostAccusePost = () => {
