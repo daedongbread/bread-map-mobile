f8f5f158acc8f2732aea67210805581dfe0b31d9
yunyeop <yykim000922@gmail.com>
2023-11-12 23:57:04 +0900

    refactor : 불필요 파일 제거

--- src/router/index.ts
+++
@@ -1,7 +0,0 @@
-// export { Navigation } from './Navigation';
-// export type {
-//   RootStackParamList,
-//   RootStackScreenProps,
-//   BakeryDetailTabParamList,
-//   BakeryDetailTabScreenProps,
-// } from './types';
