dbfd71be3e7c5c3413a23a94e10003854e4046fb
yunyeop <yykim000922@gmail.com>
2023-12-04 19:32:37 +0900

    fix : 커뮤니티 상세화면 새로고침시 댓글은 새로고침 안되는 현상 수정

--- src/containers/Community/PostDetailContainer.tsx
+++ src/containers/Community/PostDetailContainer.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import { useQueryClient } from 'react-query';
 import { useGetPost, usePostToggleLike } from '@/apis/community';
 import { PostDetailComponent } from '@/components/Community/PostDetailComponent';
 import { CommunityStackScreenProps } from '@/pages/MainStack/Community';
@@ -9,13 +10,23 @@ type Navigation = CommunityStackScreenProps<'PostDetail'>['navigation'];
 
 export const PostDetailContainer = () => {
   const navigation = useNavigation<Navigation>();
-  const { params } = useRoute<Route>();
+  const queryClient = useQueryClient();
 
+  const { params } = useRoute<Route>();
   const { postId, postTopic } = params;
 
   const { mutateAsync: onPressLike } = usePostToggleLike();
 
   const { post, refetch: refetchPost } = useGetPost({ postTopic, postId });
+
+  const refetchAll = () => {
+    // 리뷰내용 refetch
+    refetchPost();
+    // 댓글 refetch
+    queryClient.refetchQueries({
+      queryKey: ['useGetComments', { postId }],
+    });
+  };
 
   const onPressMenu = useCallback(() => {
     if (post?.writerInfo.userId) {
@@ -33,6 +44,6 @@ export const PostDetailContainer = () => {
   }
 
   return (
-    <PostDetailComponent post={post} onPressLike={onPressLike} onPressMenu={onPressMenu} refetchPost={refetchPost} />
+    <PostDetailComponent post={post} onPressLike={onPressLike} onPressMenu={onPressMenu} refetchPost={refetchAll} />
   );
 };
