2551a990cdb621c163f6462b0c168f19f91a9b4a
현승호 <>
2023-12-12 04:14:07 +0900

    feat: 인기검색어 type, 검색 결과 (bakeryDTO) type 추가

--- src/apis/search/types.ts
+++ src/apis/search/types.ts
@@ -5,3 +5,30 @@ export type RecentKeywordsEntity = {
 export type SuggestionsEntity = {
   keywordSuggestions: Array<string>;
 };
+
+export type PopularKeywordsEntity = {
+  keywordSuggestions: Array<PopularKeyword>;
+};
+
+type PopularKeyword = {
+  keyword: string;
+  rank: number;
+};
+
+export type SearchBakeryEntity = {
+  subwayStationName: string;
+  searchResultDtoList: Array<BakeryDTO>;
+};
+
+export type BakeryDTO = {
+  bakeryId: number;
+  bakeryName: string;
+  breadId: number;
+  breadName: string;
+  address: string;
+  distance: number; // 9493401.0,
+  reviewNum: number; // 0
+  totalScore: number; // 0.0
+};
+
+export type SearchType = 'POPULAR' | 'DISTANCE' | string;
